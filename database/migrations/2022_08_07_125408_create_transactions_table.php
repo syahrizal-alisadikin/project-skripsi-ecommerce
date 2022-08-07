11b11137822cfe16b8e95b1833fb6e4367306fe6
syahrizal-alisadikin <alisadikinsyahrizal@gmail.com>
2022-08-07 20:00:33 +0700

    update migration

---
+++ database/migrations/2022_08_07_125408_create_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->integer('total_price');
+            $table->enum('status',['pending','success','failed']); //unpaid/pending/shipping/success/failed
+            $table->string('resi');
+
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
