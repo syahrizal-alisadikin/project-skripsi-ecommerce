ac57f05c47ede868baf58052cd7aaf59e762bc0e
syahrizal-alisadikin <alisadikinsyahrizal@gmail.com>
2022-08-09 08:40:10 +0700

    fix select

--- resources/views/pages/product-detail.blade.php
+++ resources/views/pages/product-detail.blade.php
@@ -35,7 +35,7 @@
                 alt=""
               />
             </transition>
-            <div class="row">
+            <div class="row mt-2">
                 <div
                 class="col-12 col-md-3 col-lg-3"
                 v-for="(photo, index) in photos"
