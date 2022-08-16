3ef41f7378e2eaf252d3277a7384fe929b8d9e90
syahrizal-alisadikin <alisadikinsyahrizal@gmail.com>
2022-08-16 22:00:35 +0700

    update transaction di admin

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -14,7 +14,7 @@ class DashboardController extends Controller
         $days = Transaction::where('status', 'success')->whereDate('created_at', today())->sum('total_price');
         $month = Transaction::where('status', 'success')->whereMonth('created_at', today()->month)->sum('total_price');
         $year = Transaction::where('status', 'success')->whereYear('created_at', today()->year)->sum('total_price');
-        $user = User::count();
+        $user = User::where('roles', 'USER')->count();
         return view('pages.admin.dashboard', compact('days', 'month', 'year', 'user'));
     }
 }
